c4115626bf703d19f7657fbc24065b330387baf3
WasifKh123 <99185793+WasifKh123@users.noreply.github.com>
2023-05-21 12:52:08 +0500

    Paramedics done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Image from './components/Image';
-import MinistryofHealth from './components/MinistryofHealth'; 
 import Paramedics from './components/Paramedics';
-import Policy from './components/Policy';
 function App() {
   return (
     <div className="App" >
 
       <BrowserRouter>
         <Routes>
-          <Route path="/hospital" element={<Image/>}/>
-          <Route path="/ministry" element={<MinistryofHealth/>}/>
-          <Route path="/paramedics" element={<Paramedics/>}/>
-          <Route path="/policy" element={<Policy/>}/>
 
+          <Route path="/paramedics" element={<Paramedics/>}/>
         </Routes>
       </BrowserRouter>
     </div>
