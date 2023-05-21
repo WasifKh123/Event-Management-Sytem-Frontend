51726d8b384c44c53752322ed0bc8d385a9b8715
WasifKh123 <99185793+WasifKh123@users.noreply.github.com>
2023-05-21 12:54:17 +0500

    Hospital Done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Image from './components/Image';
-import MinistryofHealth from './components/MinistryofHealth'; 
-import Paramedics from './components/Paramedics';
 import Policy from './components/Policy';
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/hospital" element={<Image/>}/>
-          <Route path="/ministry" element={<MinistryofHealth/>}/>
-          <Route path="/paramedics" element={<Paramedics/>}/>
           <Route path="/policy" element={<Policy/>}/>
 
         </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,6 @@ function Header(props) {
                     <Navbar.Brand href="#home">Privacy Ops</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="/hospital">Hospital</Nav.Link>
-                        <Nav.Link href="/ministry">Ministry Of Health</Nav.Link>
-                        <Nav.Link href="/paramedics">Paramedics</Nav.Link>
                         <Nav.Link href="/policy">Set Policy</Nav.Link>
                     </Nav>
                 </Container>
